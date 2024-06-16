fac1ee0ab3ab76a9aff264bb7d8c8103624bb871
dotkurka <ostapkurka@gmail.com>
2024-06-16 21:56:54 +0300

    feat(backend): added weather api

--- apps/backend/src/features/weather/weather.controller.ts
+++ apps/backend/src/features/weather/weather.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+
+import { ZodValidationPipe } from 'src/features/common/pipes';
+import { WeatherDto, WeatherQueryDto } from 'src/features/weather/dto';
+import { weatherQuerySchema } from 'src/features/weather/validations';
+import { WeatherService } from 'src/features/weather/weather.service';
 
 @Controller('weather')
 export class WeatherController {
+  constructor(private readonly weatherService: WeatherService) {}
+
   @Get()
-  getWetherByCity() {
-    return 'hello';
+  async getWetherByCity(
+    @Query(new ZodValidationPipe(weatherQuerySchema)) { city }: WeatherQueryDto,
+  ): Promise<WeatherDto | null> {
+    return this.weatherService.getWeatherByCity(city);
   }
 }
--- apps/backend/src/features/weather/weather.module.ts
+++ apps/backend/src/features/weather/weather.module.ts
@@ -1,10 +1,11 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 
 import { WeatherController } from 'src/features/weather/weather.controller';
 import { WeatherService } from 'src/features/weather/weather.service';
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [WeatherController],
   providers: [WeatherService],
   exports: [],
